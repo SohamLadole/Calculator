c1ae081cfd9b1d0311fd3fec897dc113b1b656cf
SohamLadole <sohamsladole13@gmail.com>
2026-05-22 15:23:51 +0530

    Final

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let disp = "";
+let ans = document.getElementById("ans");
 let buttons = document.querySelectorAll('.buns');
+let result = 0;
 Array.from(buttons).forEach((button) => {
-    button.addEventListener('click', (e) =>{
-        console.log(e.target.innerHTML); 
+    button.addEventListener('click', (e) =>{ 
+        if(e.target.innerHTML == "="){
+            result = eval(disp);
+            ans.innerText = "";
+        }
+        else{
+            disp = disp + e.target.innerHTML;
+            ans.innerHTML = disp;
+        }
+        console.log(ans);
     })
 });
