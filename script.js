65d4518b00923635b9f6e5d6a4dd8ba35cb6a6a4
SohamLadole <sohamsladole13@gmail.com>
2026-05-22 15:46:02 +0530

    final

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ Array.from(buttons).forEach((button) => {
     button.addEventListener('click', (e) =>{ 
         if(e.target.innerHTML == "="){
             result = eval(disp);
-            ans.innerText = "";
+            ans.innerText = result;
+            disp = "";
+        }
+        else if(e.target.innerHTML == "AC"){
+            ans.innerHTML = "";
+            disp = "";
         }
         else{
             disp = disp + e.target.innerHTML;
